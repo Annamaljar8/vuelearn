cf05b0292e274a02bc67a957bc68b7efe71818f3
Annamaljar8 <annamaljar8@gmail.com>
2019-03-27 09:42:46 +0100

    end of exercise

--- Exercise_9/src/App.vue
+++ Exercise_9/src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <form>
-            <div class="row">
+            <div class="row" v-if="!submitted">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <!-- Exercise 1 -->
                     <!-- Create a Signup Form where you retrieve the following Information -->
@@ -10,56 +10,60 @@
                     <!-- Password -->
                     <!-- Store Data? Yes/No -->
 
- <div class="form-row">
-  <div class="form-group">
-    <label for="fullname">Fullname</label>
-  </div>
-  <div class="form-group col-md-6" style="padding-left: 0;">
-    <input type="text" class="form-control" placeholder="Firstname"
-    v-model="fullName.firstname">
-  </div>
-  <div class="form-group col-md-6" style="padding-right: 0;">
-    <input type="text" class="form-control" placeholder="Lastname"
-    v-model="fullName.lastname">
-  </div>
-</div>
+                    <!-- Exercise 3 -->
+                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                    <!-- which still holds the First Name and Last Name Input Field -->
+<app-fullname v-model="fullName"></app-fullname>
   <div class="form-group">
      <label for="exampleInputEmail1">Email address</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"
-    v-model="userData.email">
+    v-model="email">
   </div>
  <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"
-    v-model="userData.password">
+    v-model="password">
  </div>
  <div>
-   <p>Store Data?</p><app-switch v-model="dataSwitch"></app-switch>
+   <p>Store Data?</p>
+   <div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="Yes"
+  v-model="dataStore">
+  <label class="form-check-label" for="inlineRadio1">Yes</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="No"
+  v-model="dataStore">
+  <label class="form-check-label" for="inlineRadio2">No</label>
+</div>
  </div>
 
 
-                    <!-- Exercise 2 -->
+                  <!-- Exercise 2 -->
                     <!-- Only display the Form if it has NOT been submitted -->
                     <!-- Display the Data Summary ONCE the Form HAS been submitted -->
-
-                    <!-- Exercise 3 -->
-                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
-                    <!-- which still holds the First Name and Last Name Input Field -->
+                    <div class="row">
+                                  <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                                      <button class="btn btn-primary"
+                                              @click.prevent="submitted = true">Submit!
+                                      </button>
+                                  </div>
+                            </div>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="submitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: {{ fullName.firstname + ' ' + fullName.lastname }}</p>
-                        <p>Mail: {{ userData.email }} </p>
-                        <p>Password:{{ userData.password }} </p>
-                        <p>Store in Database?: {{ dataSwitch }}</p>
+                        <p>Full Name: {{ fullName }}</p>
+                        <p>Mail: {{ email }} </p>
+                        <p>Password:{{ password }} </p>
+                        <p>Store in Database?: {{ dataStore }}</p>
                     </div>
                 </div>
             </div>
@@ -68,24 +72,22 @@
 </template>
 
 <script>
-import Switch from './Switch.vue';
+import Fullname from './Fullname.vue';
 
     export default {
       data () {
         return {
-          fullName:{
-            firstname:'',
-            lastname:''
-          },
-          userData:{
+            fullName:'Anna Maljar',
             email:'',
-            password:''
-          },
-          dataSwitch: true
+            password:'',
+            dataStore: 'Yes',
+            submitted: false
         }
       },
+
+
       components:{
-        appSwitch: Switch,
+        appFullname: Fullname,
       }
     }
 </script>
---
+++ Exercise_9/src/Fullname.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+     <div class="form-row">
+      <div class="form-group">
+        <label for="fullname">Fullname</label>
+      </div>
+      <div class="form-group col-md-6" style="padding-left: 0;">
+        <input type="text" class="form-control" placeholder="Firstname"
+      :value="firstName" @input="nameChanged(true, $event)">
+      </div>
+      <div class="form-group col-md-6" style="padding-right: 0;">
+        <input type="text" class="form-control" placeholder="Lastname"
+        :value="lastName" @input="nameChanged(false, $event)">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+      props:['value'],
+      methods:{
+        nameChanged(isFirst, event){
+          let name='';
+          if(isFirst){
+            name=event.target.value + ' ' + this.lastName;
+          } else {
+            name=this.firstName + ' ' + event.target.value;
+          }
+          this.value=name;
+          this.$emit('input', this.value);
+        }
+      },
+      computed:{
+        firstName(){
+          return this.value.split(" ")[0];
+        },
+        lastName(){
+          return this.value.split(" ")[1];
+        },
+      }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
